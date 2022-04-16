4676d7941f860cc5504f306456df1e5306c4fc16
KIMSUBIN <KIMSUBIN@gmail.com>
2022-04-16 12:34:14 +0900

    0416-1

--- src/com/javaex/practice2/Ex05.java
+++ src/com/javaex/practice2/Ex05.java
@@ -8,13 +8,19 @@ public class Ex05 {
 		
 		System.out.println("키와 몸무게를 입력해주세요.");
 		System.out.println("키: ");
-		System.out.println("몸무게: ");
-		
 		double stature = sc.nextDouble();
-		double TTL = (stature-100)*0.9;
 		
-			if(stature>TTL) {
-				System.out.print("과체중입니다.");;
+		System.out.println("몸무게: ");
+		double weight = sc.nextDouble();
+		
+		double TTL = (stature-100)*0.9; //표준체중 공식
+		
+			if(weight>TTL) {
+				System.out.print("과체중입니다.");
+			} else if (weight<TTL) {
+				System.out.print("저체중입니다.");
+			} else {
+				System.out.print("표준체중입니다.");
 			}
 			sc.close();
 	}
--- src/com/javaex/practice2/Ex06.java
+++ src/com/javaex/practice2/Ex06.java
@@ -6,20 +6,23 @@ public class Ex06 {
 	public static void main(String[] args) {
 			Scanner sc = new Scanner(System.in);
 			System.out.println("숫자 두 개를 입력해주세요.");
-			System.out.println("숫자: ");
-			System.out.println("숫자: ");
+			System.out.print("숫자: ");
+			int count1 = sc.nextInt();
 			
+			System.out.print("숫자: ");
+			int count2 = sc.nextInt();
+			double TTL = count1*count2/2;
 			
-			int count = sc.nextInt();
-			double TTL = count*count/2;
-			
-			if (count>TTL) {
-				System.out.println("큰수: ");
-			} else {
-				System.out.println("작은수: ");
-			}
+			if (count1>TTL) { //큰수 뒤에 띄어쓰기가 안되는 이유는?
+				System.out.println("큰수: " + count1 + "작은수: " + count2 + "입니다.");
+			} else if(count1<TTL){
+				System.out.println("큰수: " + count2 + "작은수: " + count1 + "입니다.");
+			} else if(count2>TTL) {
+				System.out.println("큰수: " + count2 + "작은수: " + count1 + "입니다.");
+			}else if(count2<TTL){
+				System.out.println("큰수: " + count1 + "작은수: " + count2 + "입니다.");
+				}
 
-			sc.close();
-		
+			sc.close();	
 	}
 }
