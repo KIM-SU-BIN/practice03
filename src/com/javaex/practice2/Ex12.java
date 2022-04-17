1ae1cfb45845091b25087307877822d4966dc7d4
KIMSUBIN <KIMSUBIN@gmail.com>
2022-04-18 01:11:54 +0900

    0417-2

--- src/com/javaex/practice2/Ex12.java
+++ src/com/javaex/practice2/Ex12.java
@@ -2,17 +2,13 @@ package com.javaex.practice2;
 
 import java.util.Scanner;
 
-import javax.xml.crypto.dsig.SignatureMethod;
-
-public class Ex12 {
-	private static Object sign;
-
+public class Ex12 { 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		
+
 		System.out.println("출력되는 내용을 입력하세요.");
 		System.out.print("기호: ");
-		String mark = sc.nextLine();	
+		String sign = sc.nextLine();
 			
 		System.out.print("숫자1: ");
 		double num1 =sc.nextDouble();
@@ -26,6 +22,7 @@ public class Ex12 {
 		double mu= num1 * num2;
 		double div = num1 / num2;
 		
+		//조건문(제어문)
 		if (sign.equals("+") ) {
 			System.out.print("결과는: " + plus);
 		} else if(sign.equals("-") ) {
@@ -33,12 +30,15 @@ public class Ex12 {
 		} else if(sign.equals("*") ) {
 			System.out.print("결과는: " + mu);
 		} else if (sign.equals("/") ) {
+			if (num1 != 0) 
 			System.out.print("결과는: " + div);
-		} else {
+			else {
+				System.out.print("계산할 수 없습니다.");} }
+		else {
 			System.out.print("계산할 수 없습니다.");
 		}
-		//마무리
-		
+
+		//switch ~ break 로도 사용가능!
 		sc.close();
 	}
 }
--- src/com/javaex/practice2/Ex13.java
+++ src/com/javaex/practice2/Ex13.java
@@ -1,7 +1,20 @@
 package com.javaex.practice2;
 
+import java.util.Scanner;
+
 public class Ex13 {
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 		
+		System.out.println("숫자를 입력해주세요.");
+		System.out.print("숫자: ");
+		double ex = sc.nextDouble();
+		
+		if (ex <= 0) {
+			System.out.println( "계산결과는" + ((ex*ex*ex) - (9*ex) + 2) + " 입니다.");
+		} else {
+			System.out.println("계산결과는" + ((7*ex) +2) + " 입니다.");
+		}
+	sc.close();
 	}
 }
