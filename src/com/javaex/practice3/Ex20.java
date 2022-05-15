1a638e30aca59fce0cd8a56da95dbea7ba6e9741
subintoto <subintoto@naver.com>
2022-05-15 23:17:27 +0900

    미완성...0515

--- src/com/javaex/practice3/Ex20.java
+++ src/com/javaex/practice3/Ex20.java
@@ -1,7 +1,24 @@
 package com.javaex.practice3;
 
-public class Ex20 {
+import java.util.Scanner;
+
+public class Ex20 { //숨겨진 숫자 설정
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 		
+		int num = 0;
+		
+		System.out.println("");
+		System.out.println("============================");
+		System.out.println("[ 숫자 맞추기 게임 시작 ]");
+		System.out.println("============================");
+		int game = sc.nextInt();
+		
+		//숫자가 맞으면 게임이 종료됨 그 전까지는 게임 지속
+		if(false) {
+			System.out.println(num);
+		}
+
+		sc.close();
+		}
 	}
-}
